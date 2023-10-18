0f5c0eafbad340e85b8154a383dd2a8b898bff1c
Thomas Harrison <twh2898@vt.edu>
2023-10-18 13:14:48 -0400

    Add docs to stack

--- cc-libs/stack.lua
+++ cc-libs/stack.lua
@@ -1,5 +1,6 @@
 local stack = {}
 
+--- Create a new empty stack
 function stack:new()
     local o = {}
     setmetatable(o, self)
@@ -7,16 +8,19 @@ function stack:new()
     return o
 end
 
+--- Push a single element to the top of the stack
 function stack:push(e)
     self[#self + 1] = e
 end
 
+--- Remove and return a single element from the top of the stack
 function stack:pop()
     if #self > 0 then
         return table.remove(self, #self)
     end
 end
 
+--- Return the top element of the stack or nil if the stack is empty
 function stack:peek()
     if #self > 0 then
         return self[#self]
