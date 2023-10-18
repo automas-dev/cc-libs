38bead3bfca441d0887c8235e240410cad9c9241
Thomas Harrison <twh2898@vt.edu>
2023-10-18 14:16:52 -0400

    Add Point and some docs

--- cc-libs/map.lua
+++ cc-libs/map.lua
@@ -3,18 +3,48 @@ local logging = require 'cc-libs.logging'
 local log = logging:new('map')
 logging.MAP = log
 
-local M = {}
+local Point = {}
 
-function M:new()
+function Point:new(x, y)
     local o = {
-        graph={},
-        waypoints={},
+        id = x .. ',' .. y,
+        x = x,
+        y = y,
+        connections = {},
     }
     setmetatable(o, self)
     self.__index = self
     return o
 end
 
+--- Connect two points
+-- @parm other another Point to connect to 
+function Point:connect(other)
+    if self.connections[other.id] == nil then
+        self.connections[other.id] = other
+    end
+    if other.connections[self.id] == nil then
+        other.connections[self.id] = self
+    end
+end
+
+local M = {
+    Point = Point,
+}
+
+--- Create a new empty map
+function M:new()
+    local o = {
+        graph = {},
+        waypoints = {},
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+--- Load the map from a file
+-- @path the file to load from
 function M:load(path)
     log:debug('Dumping map to', path)
 
@@ -47,6 +77,11 @@ end
 
 function M:connect(p1, p2)
     assert(is_inline(p1, p2), 'p1 is not inline with p2')
+
+    local point = self.graph[p1]
+    if point == nil then
+        point = Point:new(p1)
+    end
 
     self.graph[p1] = p2
 end
