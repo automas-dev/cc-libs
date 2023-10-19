a8ced39f71374d4ff247ac46caa4a0517cc97c40
Thomas Harrison <twh2898@vt.edu>
2023-10-18 22:48:35 -0400

    Add astar

---
+++ cc-libs/astar.lua
@@ -0,0 +1,66 @@
+local function astar(start, goal, get_neighbors, cost_fn, heuristic_fn)
+    local open = { start }
+    local parent = {}
+    local f_score = {
+        [start] = 0
+    }
+    local h_score = {
+        [start] = f_score[start] + heuristic_fn(start, goal)
+    }
+
+    local function pop_lowest_h()
+        local lowest = nil
+        local score = nil
+        local lowest_i = nil
+        for i, key in ipairs(open) do
+            local s = h_score[key]
+            if lowest == nil or s < score then
+                lowest = key
+                score = s
+                lowest_i = i
+            end
+        end
+        table.remove(open, lowest_i)
+        return lowest
+    end
+
+    while #open > 0 do
+        -- Get node in open with lowest f + h score and remove it from open
+        local current = pop_lowest_h()
+
+        -- If the goal is found, stop searching
+        if current == goal then
+            break
+        end
+
+        -- Iterate over all neighbors of current
+        local neighbors = get_neighbors(current)
+        for _, neighbor in ipairs(neighbors) do
+            -- Calculate f score for neighbor
+            local dist = f_score[current] + cost_fn(current, neighbor)
+
+            -- If current is a shorter path to neighbor
+            if f_score[neighbor] == nil or dist < f_score[neighbor] then
+                -- Update parent and scores
+                parent[neighbor] = current
+                f_score[neighbor] = dist
+                h_score[neighbor] = dist + heuristic_fn(neighbor, goal)
+
+                -- Push neighbor into open list
+                open[#open + 1] = neighbor
+            end
+        end
+    end
+
+    local path = {}
+    local curr = goal
+    while curr ~= nil do
+        path[#path + 1] = curr
+        curr = parent[curr]
+    end
+
+    -- Returns path from goal to start
+    return path
+end
+
+return astar
