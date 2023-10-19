e066a585b9db2f68714ad608cd7686c961a69749
Thomas Harrison <twh2898@vt.edu>
2023-10-19 00:46:26 -0400

    Increase height when placing stairs

--- stairs.lua
+++ stairs.lua
@@ -10,7 +10,10 @@ if #args < 1 then
     print()
     print('Options:')
     print('    n: number of steps')
-    print('    place stairs: true will place stairs from slot 1, limits n to the number of items in slot 1')
+    print('    place stairs: true will place stairs from slot 1')
+    print()
+    print('If stairs are being placed, n will be limited to the number of stairs in slot 1.')
+    print('The height mined will also be increased to account for the steps')
     return
 end
 
@@ -20,7 +23,7 @@ local place_stairs = args[2] == 'true' or args[2] == 'yes'
 log:info('Starting with parameters n=', n)
 
 log:info('Starting fuel level', turtle.getFuelLevel())
-local fuel_need = n * 2 * 2
+local fuel_need = n * 4 * 2
 log:debug('Fuel needed is', fuel_need)
 if turtle.getFuelLevel() < fuel_need then
     log:fatal('Not enough fuel! Need', fuel_need)
@@ -82,7 +85,7 @@ local function try_up()
     end
 end
 
-local function dig_forward()
+for i = 1, n do
     if turtle.getFuelLevel() == 0 then
         log:fatal('Ran out of fuel!')
     end
@@ -90,13 +93,19 @@ local function dig_forward()
     turtle.dig()
     try_forward()
     turtle.digUp()
+
+    if place_stairs and i % 2 == 1 then
+        try_up()
+        turtle.digUp()
+        if i < n then
+            turtle.dig()
+        end
+        try_down()
+    end
+
     turtle.digDown()
     try_down()
     turtle.digDown()
-end
-
-for _ = 1, n do
-    dig_forward()
 end
 
 -- Return
