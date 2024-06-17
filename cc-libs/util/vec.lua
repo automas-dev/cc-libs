d1f24370f0a1abf30d3ce0d0fa7355099f0a96db
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-06-16 22:27:28 -0400

    Add vec3, check for 0 before dividing

--- cc-libs/util/vec.lua
+++ cc-libs/util/vec.lua
@@ -81,6 +81,9 @@ end
 
 function vec2:set_length(new_length)
     local length = self:get_length()
+    if length == 0 then
+        return
+    end
     self.x = self.x * new_length / length
     self.y = self.y * new_length / length
 end
@@ -122,12 +125,148 @@ function vec2:get_angle_between(other)
 end
 
 function vec2:normalized()
+    local new_vec = vec2:new(self.x, self.y)
+    new_vec.set_length(1)
+    return new_vec
+end
+
+local vec3 = {}
+
+function vec3:new(x, y, z)
+    if y == nil then
+        y = x
+        z = x
+    end
+    local o = {
+        x = x or 0,
+        y = y or 0,
+        z = z or 0,
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+vec3.__add = function(a, b)
+    return vec3:new(a.x + b.x, a.y + b.y, a.z + b.z)
+end
+
+vec3.__sub = function(a, b)
+    return vec3:new(a.x - b.x, a.y - b.y, a.z - b.z)
+end
+
+vec3.__mul = function(a, b)
+    return vec3:new(a.x * b.x, a.y * b.y, a.z * b.z)
+end
+
+vec3.__div = function(a, b)
+    return vec3:new(a.x / b.x, a.y / b.y, a.z / b.z)
+end
+
+vec3.__mod = function(a, b)
+    return vec3:new(a.x % b.x, a.y % b.y, a.z % b.z)
+end
+
+vec3.__pow = function(a, b)
+    return vec3:new(a.x ^ b.x, a.y ^ b.y, a.z ^ b.z)
+end
+
+vec3.__len = function()
+    return 2
+end
+
+vec3.__eq = function(a, b)
+    return a.x == b.x and a.y == b.y and a.z == b.z
+end
+
+vec3.__index = function(a, key)
+    print(a, key)
+    if key == 0 then
+        return a.x
+    elseif key == 1 then
+        return a.y
+    elseif key == 2 then
+        return a.z
+    else
+        return nil
+    end
+end
+
+vec3.__newindex = function(a, key, value)
+    if key == 0 then
+        a.x = value
+    elseif key == 1 then
+        a.y = value
+    elseif key == 2 then
+        a.z = value
+    end
+end
+
+vec3.__tostring = function(a)
+    return 'vec3(' .. a.x .. ', ' .. a.y .. ', ' .. a.z .. ')'
+end
+
+function vec3:get_length()
+    if self.x == 0 and self.y == 0 and self.z == 0 then
+        return 0
+    else
+        return math.sqrt(self.x ^ 2 + self.y ^ 2 + self.z ^ 2)
+    end
+end
+
+function vec3:set_length(new_length)
     local length = self:get_length()
-    -- if length != 0 then
-    --     return 
-    -- end
+    if length == 0 then
+        return
+    end
+    self.x = self.x * new_length / length
+    self.y = self.y * new_length / length
+    self.z = self.z * new_length / length
+end
+
+-- function vec3:rotate(angle_deg)
+--     local rad = math.rad(angle_deg)
+--     local cos = math.cos(rad)
+--     local sin = math.sin(rad)
+--     local new_x = self.x * cos - self.y * sin
+--     local new_y = self.x * sin + self.y * cos
+--     self.x = new_x
+--     self.y = new_y
+-- end
+
+-- function vec3:rotated(angle_deg)
+--     local new_vec = vec3:new(self.x, self.y)
+--     new_vec.rotate(angle_deg)
+--     return new_vec
+-- end
+
+-- function vec3:get_angle()
+--     if self:get_length() == 0 then
+--         return 0
+--     else
+--         return math.deg(math.atan(self.y, self.x))
+--     end
+-- end
+
+-- function vec3:set_angle(angle_deg)
+--     self.x = self:get_length()
+--     self.y = 0
+--     self.rotate(angle_deg)
+-- end
+
+-- function vec3:get_angle_between(other)
+--     local cross = self.x * other.y - self.y * other.x
+--     local dot = self.x * other.x + self.y * other.y
+--     return math.deg(math.atan(cross, dot))
+-- end
+
+function vec3:normalized()
+    local new_vec = vec3:new(self.x, self.y, self.z)
+    new_vec.set_length(1)
+    return new_vec
 end
 
 return {
     Vec2 = vec2,
+    Vec3 = vec3,
 }
