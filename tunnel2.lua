5213376556846015dfd4810b98c4be970fb197e1
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-06-22 23:44:26 -0400

    Add tunnel2

---
+++ tunnel2.lua
@@ -0,0 +1,36 @@
+local logging = require 'cc-libs.util.logging'
+logging.file = 'tunnel2.log'
+logging.level = logging.Levels.info
+logging.file_level = logging.Levels.debug
+local log = logging.get_logger('main')
+
+local actions = require 'cc-libs.actions'
+
+local args = { ... }
+if #args < 1 then
+    print('Usage: tunnel2 <length> [return|false]')
+    print()
+    print('Options:')
+    print('    length: length of the tunnel')
+    print('    return: return to the start after finishing the tunnel')
+    return
+end
+
+local length = tonumber(args[1])
+local end_return = args[2] == 'true' or args[2] == 'yes'
+
+log:info('Starting with parameters length=', length,
+    'return=', end_return)
+
+turtle.up()
+
+actions.dig_forward(length)
+
+if end_return then
+    turtle.left()
+    turtle.left()
+    actions.try_forward(length)
+    turtle.left()
+    turtle.left()
+    turtle.down()
+end
