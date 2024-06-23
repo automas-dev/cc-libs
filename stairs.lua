f37f72adfd86b0044298227cd45ddde337b8a6ca
Thomas Harrison <twh2898@vt.edu>
2024-06-23 17:33:19 -0400

    Use new motion module in stairs.lua

--- stairs.lua
+++ stairs.lua
@@ -2,6 +2,9 @@ local logging = require 'cc-libs.util.logging'
 logging.file = 'stairs.log'
 local log = logging.get_logger('main')
 
+---@module 'ccl_motion'
+local ccl_motion = require 'cc-libs.turtle.motion'
+
 local MOVE_MAX_TRIES = 10
 
 local args = { ... }
@@ -34,79 +37,29 @@ if place_stairs and turtle.getItemCount(1) < n then
     log:fatal('Not enough stairs in slot 1, need', n)
 end
 
-local function try_forward()
-    local did_move = false
-    for _ = 1, MOVE_MAX_TRIES do
-        if turtle.forward() then
-            did_move = true
-            break
-        else
-            log:debug('Could not move forward, trying to dig')
-            turtle.dig()
-        end
-    end
+local tmc = ccl_motion.Motion:new()
+tmc:enable_dig()
 
-    if not did_move then
-        log:fatal('Failed to move forward after', MOVE_MAX_TRIES, 'attempts')
-    end
-end
-
-local function try_down()
-    local did_move = false
-    for _ = 1, MOVE_MAX_TRIES do
-        if turtle.down() then
-            did_move = true
-            break
-        else
-            log:debug('Could not move down, trying to dig down')
-            turtle.digDown()
-        end
-    end
-
-    if not did_move then
-        log:fatal('Failed to move down after', MOVE_MAX_TRIES, 'attempts')
-    end
-end
-
-local function try_up()
-    local did_move = false
-    for _ = 1, MOVE_MAX_TRIES do
-        if turtle.up() then
-            did_move = true
-            break
-        else
-            log:debug('Could not move up, trying to dig down')
-            turtle.digUp()
-        end
-    end
-
-    if not did_move then
-        log:fatal('Failed to move up after', MOVE_MAX_TRIES, 'attempts')
-    end
-end
-
-turtle.up()
+tmc:up()
 
 for i = 1, n do
     if turtle.getFuelLevel() == 0 then
         log:fatal('Ran out of fuel!')
     end
 
-    turtle.dig()
-    try_forward()
+    tmc:forward()
     turtle.digUp()
 
     if place_stairs and i % 2 == 1 then
-        try_up()
+        tmc:up()
         turtle.digUp()
         if i < n then
             turtle.dig()
         end
-        try_down()
+        tmc:down()
     end
 
-    turtle.digDown()
-    try_down()
+    tmc:down()
     turtle.digDown()
 end
 
@@ -114,19 +67,17 @@ end
 
 log:info('Returning to station')
 
-turtle.turnRight()
-turtle.turnRight()
+tmc:around()
 
 for _ = 1, n do
     if place_stairs then
         turtle.placeDown()
     end
-    try_up()
-    try_forward()
+    tmc:up()
+    tmc:forward()
 end
 
-turtle.turnRight()
-turtle.turnRight()
-turtle.down()
+tmc:around()
+tmc:down()
 
 log:info('Done!')
