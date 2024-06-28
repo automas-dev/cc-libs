8243c6b31dc3f58514c4d862973383494e299b0e
Thomas Harrison <theharrisoncrafter@gmail.com>
2024-06-27 22:01:35 -0400

    Add select_slot, change return of find_slot for not found

--- cc-libs/turtle/actions.lua
+++ cc-libs/turtle/actions.lua
@@ -1,30 +1,54 @@
+---@meta ccl_actions
+
 local logging = require 'cc-libs.util.logging'
 local log = logging.get_logger('actions')
 
 local M = {}
 
+---Find the first slot with at least `need` items of the given name.
+---@param item_name string minecraft item id
+---@param need integer 1 to 64
+---@return integer|nil
 function M.find_slot(item_name, need)
     need = need or 1
 
-    local torch_slot = 0
+    local item_slot = nil
     for i = 1, 16 do
         if turtle.getItemDetail(i).name == item_name then
-            torch_slot = i
+            item_slot = i
         end
     end
 
-    if torch_slot == 0 then
+    if item_slot == nil then
         log:info('Item', item_name, 'was not found in inventory')
-    elseif turtle.getItemCount(torch_slot) < need then
+    elseif turtle.getItemCount(item_slot) < need then
         log:info('Not enough of', item_name, 'found in inventory')
-        torch_slot = 0
+        item_slot = nil
     end
 
-    return torch_slot
+    return item_slot
 end
 
-function M.find_torch(need)
-    return M.find_slot('minecraft:torch', need)
+---Find the first slot with at least 1 torch.
+---@return integer|nil
+function M.find_torch()
+    return M.find_slot('minecraft:torch', 1)
+end
+
+---Find and select the first slot with an item with the given minecraft id
+---@param item_name string minecraft item id
+---@return integer|nil item_slot slot number if selected
+function M.select_slot(item_name)
+    local item_slot = M.find_slot(item_name, 1)
+
+    if item_slot == nil then
+        log:warning('Item', item_name, 'was not found in inventory')
+        return nil
+    end
+
+    turtle.select(item_slot)
+
+    return item_slot
 end
 
 function M.assert_fuel(need)
