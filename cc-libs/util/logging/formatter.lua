922e9ad2b81f3f17e4fc28de76fdfa93d217d456
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-03-06 00:05:24 -0500

    Add formatters

---
+++ cc-libs/util/logging/formatter.lua
@@ -0,0 +1,101 @@
+local json = require 'cc-libs.util.json'
+
+---Get a string timestamp for the current time
+---@param time? number
+---@return string
+local function time_only(time)
+    ---@diagnostic disable-next-line: return-type-mismatch
+    return os.date('%H:%M:%S', time)
+end
+
+---Get a string timestamp for the current date and time
+---@param time? number
+---@return string
+local function datetime(time)
+    ---@diagnostic disable-next-line: return-type-mismatch
+    return os.date('%Y-%m-%dT%H:%M:%S', time)
+end
+
+---@class Record
+---@field subsystem string
+---@field level string
+---@field location string
+---@field message string
+---@field time number
+local Record = {}
+
+---@param subsystem string
+---@param level number|LogLevel
+---@param location string
+---@param message string
+function Record:new(subsystem, level, location, message, time)
+    local o = {
+        subsystem = subsystem,
+        level = level,
+        location = location,
+        message = message,
+        time = time,
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+---@class Formatter
+local Formatter = {}
+
+function Formatter:new()
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+---@param record Record
+function Formatter:format_record(record)
+    return record.message
+end
+
+---@class ShortFormatter : Formatter
+local ShortFormatter = Formatter:new()
+
+function ShortFormatter:format_record(record)
+    return '[' .. record.subsystem .. '] ' .. record.message
+end
+
+---@class LongFormatter : Formatter
+local LongFormatter = Formatter:new()
+
+function LongFormatter:format_record(record)
+    return '['
+        .. datetime(record.time)
+        .. '] ['
+        .. record.subsystem
+        .. '] ['
+        .. record.location
+        .. '] ['
+        .. record.level
+        .. '] '
+        .. record.message
+end
+
+---@class JsonFormatter : Formatter
+local JsonFormatter = Formatter:new()
+
+function JsonFormatter:format_record(record)
+    return json.encode({
+        timestamp = datetime(record.time),
+        subsystem = record.subsystem,
+        location = record.location,
+        level = record.level,
+        message = record.message,
+    })
+end
+
+return {
+    Formatter = Formatter,
+    Record = Record,
+    ShortFormatter = ShortFormatter,
+    LongFormatter = LongFormatter,
+    JsonFormatter = JsonFormatter
+}
