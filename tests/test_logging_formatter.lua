99c55aedb9254d1225453911490b9af023f09587
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-03-08 00:04:42 -0500

    Test json formatter

--- tests/test_logging_formatter.lua
+++ tests/test_logging_formatter.lua
@@ -1,8 +1,11 @@
+local json = require 'cc-libs.util.json'
+
 local formatter = require 'cc-libs.util.logging.formatter'
 local Record = formatter.Record
 local Formatter = formatter.Formatter
 local ShortFormatter = formatter.ShortFormatter
 local LongFormatter = formatter.LongFormatter
+local JsonFormatter = formatter.JsonFormatter
 
 local test = {}
 
@@ -32,6 +35,19 @@ function test.long_formatter()
     local r = Record:new('ss', 1, 'lc', 'msg', 1741354307)
     local local_date = os.date('%Y-%m-%dT%H:%M:%S', 1741354307)
     expect_eq('[' .. local_date .. '] [ss] [lc] [debug] msg', f:format_record(r))
+end
+
+function test.json_formatter()
+    local f = JsonFormatter:new()
+    local r = Record:new('ss', 1, 'lc', 'msg', 1741354307)
+    local local_date = os.date('%Y-%m-%dT%H:%M:%S', 1741354307)
+    local text = f:format_record(r)
+    local decoded = json.decode(text)
+    expect_eq(local_date, decoded.timestamp)
+    expect_eq('ss', decoded.subsystem)
+    expect_eq('lc', decoded.location)
+    expect_eq('debug', decoded.level)
+    expect_eq('msg', decoded.message)
 end
 
 return test
