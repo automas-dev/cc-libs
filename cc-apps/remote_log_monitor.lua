6ac994fc8be258f8d71639cf2f0507cfe17bf65e
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-03-10 22:17:50 -0400

    Decode json log message

--- cc-apps/remote_log_monitor.lua
+++ cc-apps/remote_log_monitor.lua
@@ -1,8 +1,21 @@
 package.path = '../?.lua;../?/init.lua;' .. package.path
 
+local json = require 'cc-libs.util.json'
+local logging = require 'cc-libs.util.logging'
+
 peripheral.find('modem', rednet.open)
 
+local fmt = logging.ShortFormatter:new()
+local stream = logging.ConsoleStream:new()
+
 while true do
+    ---@type number, string
     local id, message = rednet.receive('remote_log')
-    print('Got message from ' .. id .. ': ' .. message)
+    ---@type boolean, Record
+    local success, data = pcall(json.decode, message)
+    if not success then
+        print('Failed to decode message from ' .. id)
+    else
+        stream:send('[' .. data['host'] .. '] ' .. fmt:format_record(data))
+    end
 end
