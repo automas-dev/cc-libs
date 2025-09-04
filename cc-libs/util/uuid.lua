dd3b78006ae83ecdf499f78c3dddd9543d6ebcdb
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-09-03 20:09:03 -0400

    Fix result of uuid

--- cc-libs/util/uuid.lua
+++ cc-libs/util/uuid.lua
@@ -1,14 +1,15 @@
 -- https://gist.github.com/jrus/3197011
 
-local random = math.random
 math.randomseed(os.time())
 
 local function uuid()
     local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    return string.gsub(template, '[xy]', function(c)
-        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+    -- gsub returns 2 values, string and count
+    local res = string.gsub(template, '[xy]', function(c)
+        local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
         return string.format('%x', v)
     end)
+    return res
 end
 
 return uuid
