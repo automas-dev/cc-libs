9d5bd3a2e651ff92a45744194e329f41717572df
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-09-07 00:24:05 -0400

    Rewrite lumber.lua

--- cc-apps/lumber.lua
+++ cc-apps/lumber.lua
@@ -2,56 +2,46 @@ package.path = '../?.lua;../?/init.lua;' .. package.path
 local logging = require 'cc-libs.util.logging'
 logging.basic_config {
     level = logging.Level.INFO,
-    file_level = logging.Level.DEBUG,
+    file_level = logging.Level.TRACE,
     filepath = 'logs/lumber.log',
 }
-logging.file_level = logging.Level.DEBUG
 local log = logging.get_logger('main')
 
----@module 'ccl_motion'
 local ccl_motion = require 'cc-libs.turtle.motion'
+local Motion = ccl_motion.Motion
 
-local log_types = {
-    ['minecraft:oak_log'] = true,
-    ['minecraft:spruce_log'] = true,
-    ['minecraft:birch_log'] = true,
-    ['minecraft:jungle_log'] = true,
-    ['minecraft:acacia_log'] = true,
-    ['minecraft:dark_oak_log'] = true,
-    ['minecraft:mangrove_log'] = true,
-    ['minecraft:cherry_log'] = true,
-    ['minecraft:crimson_stem'] = true,
-    ['minecraft:warped_stem'] = true,
-    ['minecraft:stripped_oak_log'] = true,
-    ['minecraft:stripped_spruce_log'] = true,
-    ['minecraft:stripped_birch_log'] = true,
-    ['minecraft:stripped_jungle_log'] = true,
-    ['minecraft:stripped_acacia_log'] = true,
-    ['minecraft:stripped_dark_oak_log'] = true,
-    ['minecraft:stripped_mangrove_log'] = true,
-    ['minecraft:stripped_cherry_log'] = true,
-    ['minecraft:stripped_crimson_stem'] = true,
-    ['minecraft:stripped_warped_stem'] = true,
-}
-
-local tmc = ccl_motion.Motion:new()
+local tmc = Motion:new()
 tmc:enable_dig()
 
-local height = 0
-
-while true do
+local function is_log()
     local exists, info = turtle.inspect()
-    if not exists or not log_types[info.name] then
-        break
+    if exists then
+        local name = info.name
+        return string.find(name, 'log') ~= nil
     end
-    turtle.dig()
-    tmc:up()
-    height = height + 1
+    return false
 end
 
--- Return
+local function harvest()
+    log:info('Starting harvest')
+    local height = 0
+    while is_log() do
+        log:trace('Mining log at height', height)
+        turtle.dig()
+        tmc:up()
+        height = height + 1
+    end
+    log:info('Finished mining', height, 'logs')
+    tmc:down(height)
+end
 
-log:info('Returning to station')
-tmc:down(height)
+local function run()
+    while true do
+        if is_log() then
+            harvest()
+        end
+        sleep(1)
+    end
+end
 
-log:info('Done!')
+log:log_errors(run)
