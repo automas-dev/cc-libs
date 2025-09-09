1660750a991b998c1369829c502964e23e8fa65b
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-09-09 19:58:40 -0400

    Location (#55)
    * Add unary minus operator to vec docstring
    * Handle format of error message
    * Use new motion controller in harvest_crops
    * Add return_home function to harvest_crops

--- cc-apps/harvest_crops.lua
+++ cc-apps/harvest_crops.lua
@@ -7,6 +7,9 @@ logging.basic_config {
 }
 local log = logging.get_logger('main')
 
+local vec = require 'cc-libs.util.vec'
+local vec3 = vec.vec3
+
 local argparse = require 'cc-libs.util.argparse'
 local parser = argparse.ArgParse:new('farm', 'Harvest crops from the bottom right corner')
 parser:add_arg('width', { help = 'plot width' })
@@ -18,18 +21,61 @@ local length = tonumber(args.length)
 
 log:info('Start farm with width=', width, 'length=', length)
 
-local ccl_motion = require 'cc-libs.turtle.motion'
-local Motion = ccl_motion.Motion
+local ccl_motion = require 'cc-libs.turtle.controller'
+local MotionController = ccl_motion.MotionController
+
+local ccl_location = require 'cc-libs.turtle.location'
+local Compass = ccl_location.Compass
 
 local actions = require 'cc-libs.turtle.actions'
 
-local tmc = Motion:new()
+local tmc = MotionController:new()
+tmc.location.debug_location = true
 
 local seed_name_map = {
     ['minecraft:wheat'] = 'minecraft:wheat_seeds',
     ['minecraft:potatoes'] = 'minecraft:potato',
     ['minecraft:carrots'] = 'minecraft:carrot',
 }
+
+local home = vec3(1564, 69, -699)
+
+local function return_home()
+    local pos = tmc.location.pos
+    local delta = home - pos
+
+    log:info('Returning to home')
+    log:debug('Delta from', pos, 'to home', home, 'is', delta)
+
+    if delta.z > 0 then
+        tmc:face(Compass.SOUTH)
+    elseif delta.z < 0 then
+        tmc:face(Compass.NORTH)
+    end
+
+    tmc:forward(math.abs(delta.z))
+
+    log:trace('Finished z axis motion')
+
+    if delta.x > 0 then
+        tmc:face(Compass.EAST)
+    elseif delta.x < 0 then
+        tmc:face(Compass.WEST)
+    end
+
+    tmc:forward(math.abs(delta.x))
+    tmc:face(Compass.SOUTH)
+
+    log:trace('Finished x axis motion')
+
+    if pos.y > home.y then
+        tmc:down(pos.y - home.y)
+    elseif pos.y < home.y then
+        log:fatal('Why are you underground?')
+    end
+
+    log:trace('Finished y axis motion')
+end
 
 local function replant(name)
     log:debug('Replant', name)
@@ -88,10 +134,7 @@ local function harvest_crops()
 
     log:info('Finished harvest')
 
-    tmc:left()
-    tmc:forward(width - 1)
-    tmc:left()
-    tmc:down()
+    return_home()
 
     for i = 1, 16 do
         actions.dump_slot(i, 'down')
@@ -101,6 +144,21 @@ local function harvest_crops()
 end
 
 local function run()
+    if tmc.location.has_fix and tmc.location.pos ~= home then
+        log:warning('Did not start at home, may have been unloaded')
+        if not turtle.detect() then
+            tmc:forward()
+        else
+            tmc:backward()
+        end
+        if not tmc.location.has_heading then
+            log:fatal('Could not acquire heading to return home')
+        else
+            log:info('Returning to home before starting harvest')
+            return_home()
+        end
+    end
+
     while true do
         harvest_crops()
         sleep(60 * 20)
