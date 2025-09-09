f90d904557e7794e954670cc903e866880ac50e9
Thomas Harrison <theharrisoncrafter@gmail.com>
2025-09-08 21:42:34 -0400

    Reduce logging

--- cc-apps/mainframe.lua
+++ cc-apps/mainframe.lua
@@ -6,49 +6,51 @@ logging.basic_config {
     filepath = 'logs/mainframe.log',
 }
 local log = logging.get_logger('main')
+local tel_log = logging.get_logger('telemetry')
+local remote_log = logging.get_logger('remote_log')
 
 local json = require 'cc-libs.util.json'
 
 local turtle_states = {}
 
 local function proto_telemetry(id, message)
-    log:info('proto telemetry')
+    tel_log:debug('proto telemetry')
     local success, data = pcall(json.decode, message)
-    log:debug('Finish decode')
+    tel_log:debug('Finish decode')
     if not success then
-        log:error('Failed to decode message from', id)
+        tel_log:error('Failed to decode message from', id)
         return
     end
     if not turtle_states[id] then
-        log:debug('Found turtle for id', id)
+        tel_log:debug('Found turtle for id', id)
         turtle_states[id] = {
             id = id,
         }
     end
-    log:debug('Updating state')
+    tel_log:debug('Updating state')
     for k, v in pairs(data) do
         turtle_states[id][k] = v
     end
 end
 
 local function proto_report(id, message)
-    log:info('proto report')
+    tel_log:debug('proto report')
     local success, data = pcall(json.decode, message)
     local response = nil
     if not success then
-        log:error('Failed to decode message from', id)
+        tel_log:error('Failed to decode message from', id)
         response = {
             ok = false,
             err = 'Bad message',
         }
     elseif not data.id then
-        log:debug('Missing id', data)
+        tel_log:debug('Missing id', data)
         response = {
             ok = false,
             err = 'Missing id',
         }
     elseif not turtle_states[data.id] then
-        log:debug('Unknown id', data.id)
+        tel_log:debug('Unknown id', data.id)
         response = {
             ok = false,
             err = 'Unknown id',
@@ -60,7 +62,7 @@ local function proto_report(id, message)
             status = turtle_states[data.id],
         }
     end
-    log:info('Respond to', id)
+    tel_log:debug('Respond to', id)
     rednet.send(id, json.encode(response), 'mainframe_response')
 end
 
@@ -73,15 +75,15 @@ local function run_telemetry()
     log:info('Starting telemetry thread')
 
     peripheral.find('modem', rednet.open)
-    log:debug('Rednet open')
+    tel_log:debug('Rednet open')
 
     while true do
         local id, message, protocol = rednet.receive()
-        log:info('Got message', message, 'from id', id, 'with protocol', protocol)
+        tel_log:trace('Got message', message, 'from id', id, 'with protocol', protocol)
         local proto = protocols[protocol]
         if proto then
-            log:debug('Found handler for protocol', protocol)
-            log:catch_errors(proto, id, message)
+            tel_log:debug('Found handler for protocol', protocol)
+            tel_log:catch_errors(proto, id, message)
         end
     end
 end
@@ -98,7 +100,7 @@ local function run_remote_log()
         local id, message = rednet.receive('remote_log')
         local success, data = pcall(json.decode, message)
         if not success then
-            log:error('Failed to decode message from', id)
+            remote_log:error('Failed to decode message from', id)
         else
             if logging.level_from_name(data['level']) >= logging.Level.WARNING then
                 stream:send('[' .. data['host'] .. '] ' .. fmt:format_record(data))
