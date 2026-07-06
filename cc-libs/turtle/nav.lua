855eb357dfc05b02c815541757d187aa4b150478
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-06 13:41:11 -0400

    cleanup

--- cc-libs/turtle/nav.lua
+++ cc-libs/turtle/nav.lua
@@ -3,13 +3,10 @@ local log = logging.get_logger('nav')
 
 local ccl_map = require 'cc-libs.map'
 local Point = ccl_map.Point
+local Map = ccl_map.Point
 
 local ccl_location = require 'cc-libs.turtle.location'
-local Compass = ccl_location.Compass
-
-local table_size = require 'cc-libs.util.table_size'
-
-local astar = require 'cc-libs.astar'
+local Location = ccl_location.Location
 
 ---@class Nav
 ---@field map Map
@@ -72,76 +69,76 @@ function Nav:find_path(start_poi_name, goal_poi_name)
     return self.map:find_path(start, goal)
 end
 
----Move to the trace step
----@param step Vec3 position to move to
-function Nav:trace_step(step)
-    log:debug('trace step to pos', step.x, step.y, step.z)
-    local pos = self.gps.pos
-    assert(is_inline(pos, step), 'Step is not inline with current position')
-    log:trace('trace starts at pos', pos.x, pos.y, pos.z)
+-- ---Move to the trace step
+-- ---@param step Vec3 position to move to
+-- function Nav:trace_step(step)
+--     log:debug('trace step to pos', step.x, step.y, step.z)
+--     local pos = self.gps.pos
+--     assert(is_inline(pos, step), 'Step is not inline with current position')
+--     log:trace('trace starts at pos', pos.x, pos.y, pos.z)
 
-    if pos.x ~= step.x then
-        local delta = math.abs(step.x - pos.x)
+--     if pos.x ~= step.x then
+--         local delta = math.abs(step.x - pos.x)
 
-        if pos.x < step.x then
-            self:face(Compass.EAST)
-        else
-            self:face(Compass.WEST)
-        end
+--         if pos.x < step.x then
+--             self:face(Compass.EAST)
+--         else
+--             self:face(Compass.WEST)
+--         end
 
-        self.motion:forward(delta)
-    elseif pos.y ~= step.y then
-        local delta = math.abs(step.y - pos.y)
+--         self.motion:forward(delta)
+--     elseif pos.y ~= step.y then
+--         local delta = math.abs(step.y - pos.y)
 
-        if pos.y < step.y then
-            self.motion:up(delta)
-        else
-            self.motion:down(delta)
-        end
-    elseif pos.z ~= step.z then
-        local delta = math.abs(step.z - pos.z)
+--         if pos.y < step.y then
+--             self.motion:up(delta)
+--         else
+--             self.motion:down(delta)
+--         end
+--     elseif pos.z ~= step.z then
+--         local delta = math.abs(step.z - pos.z)
 
-        if pos.z < step.z then
-            self:face(Compass.NORTH)
-        else
-            self:face(Compass.SOUTH)
-        end
-        self.motion:forward(delta)
-    end
+--         if pos.z < step.z then
+--             self:face(Compass.NORTH)
+--         else
+--             self:face(Compass.SOUTH)
+--         end
+--         self.motion:forward(delta)
+--     end
 
-    local end_pos = self.gps.pos
-    local at_end_pos = end_pos.x == step.x and end_pos.y == step.y and end_pos.z == step.z
+--     local end_pos = self.gps.pos
+--     local at_end_pos = end_pos.x == step.x and end_pos.y == step.y and end_pos.z == step.z
 
-    assert(at_end_pos, 'trace_step did not reach step position')
-end
+--     assert(at_end_pos, 'trace_step did not reach step position')
+-- end
 
-function Nav:follow()
-    log:info('Going to resume point')
-    assert(self.resume ~= nil, 'No resume point is marked')
-    assert(self.gps.pos == self.station, 'Not aligned with trace start')
+-- function Nav:follow()
+--     log:info('Going to resume point')
+--     assert(self.resume ~= nil, 'No resume point is marked')
+--     assert(self.gps.pos == self.station, 'Not aligned with trace start')
 
-    local path = self:find_path(self.station, self.resume)
+--     local path = self:find_path(self.station, self.resume)
 
-    log:debug('Path has', #path, 'points')
+--     log:debug('Path has', #path, 'points')
 
-    for i = 1, #path do
-        self:trace_step(path[i])
-    end
-end
+--     for i = 1, #path do
+--         self:trace_step(path[i])
+--     end
+-- end
 
-function Nav:back_follow()
-    log:info('Going to station point')
-    assert(self.resume ~= nil, 'No resume point is marked')
-    assert(self.gps.pos == self.resume, 'Not aligned with trace end')
+-- function Nav:back_follow()
+--     log:info('Going to station point')
+--     assert(self.resume ~= nil, 'No resume point is marked')
+--     assert(self.gps.pos == self.resume, 'Not aligned with trace end')
 
-    local path = self:find_path(self.resume, self.station)
+--     local path = self:find_path(self.resume, self.station)
 
-    log:debug('Path has', #path, 'points')
+--     log:debug('Path has', #path, 'points')
 
-    for i = 1, #path do
-        self:trace_step(path[i])
-    end
-end
+--     for i = 1, #path do
+--         self:trace_step(path[i])
+--     end
+-- end
 
 local M = {
     Nav = Nav,
