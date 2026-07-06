779e756ef05b6fd7d2db4008f76a47d9b6594849
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-06 11:41:10 -0400

    Add tests for map

--- cc-libs/map.lua
+++ cc-libs/map.lua
@@ -1,9 +1,12 @@
----@module 'ccl_vec'
-
-local json = require 'cc-libs.util.json'
 local logging = require 'cc-libs.util.logging'
 local log = logging.get_logger('map')
+
+local json = require 'cc-libs.util.json'
+
 local table_size = require 'cc-libs.util.table_size'
+
+local ccl_vec = require 'cc-libs.util.vec'
+local Vec3 = ccl_vec.Vec3
 
 ---@alias PointId string
 
@@ -32,6 +35,7 @@ local Point = {}
 function Point:new(x, y, z)
     local o = {
         id = point_id(x, y, z),
+        -- TODO vec instead of x, y, z
         x = x,
         y = y,
         z = z,
@@ -42,6 +46,13 @@ function Point:new(x, y, z)
     return o
 end
 
+---Create a Point from a Vec3
+---@param vec Vec3
+---@return Point
+function Point:from_vec3(vec)
+    return Point:new(vec.x, vec.y, vec.z)
+end
+
 ---Connect this point to another. This will create the link on both points to each other.
 ---@param other Point point to link with
 ---@param weight? number weight of this link
@@ -50,6 +61,21 @@ function Point:link(other, weight)
     self.links[other.id] = weight
     if other.links[self.id] == nil then
         other:link(self, weight)
+    end
+end
+
+---Check if another point is inline with one of this points axis (ie. 2 axes match)
+---@param other Vec3|Point
+---@return boolean
+function Point:inline(other)
+    if self.x ~= other.x then
+        return self.y == other.y and self.z == other.z
+    elseif self.y ~= other.y then
+        return self.x == other.x and self.z == other.z
+    elseif self.z ~= other.z then
+        return self.x == other.x and self.y == other.y
+    else
+        return true
     end
 end
 
@@ -73,6 +99,7 @@ end
 local Map = {}
 
 --- Create a new empty map
+---@return Map
 function Map:new()
     local o = {
         graph = {},
@@ -102,22 +129,6 @@ function Map:dump(path)
     local file = assert(io.open(path, 'w'))
     file:write(json.encode(self))
     file:close()
-end
-
----Check if two points share the same value for at least 2 axis
----@param pos1 Vec3|Point
----@param pos2 Vec3|Point
----@return boolean
-local function is_inline(pos1, pos2)
-    if pos1.x ~= pos2.x then
-        return pos1.y == pos2.y and pos1.z == pos2.z
-    elseif pos1.y ~= pos2.y then
-        return pos1.x == pos2.x and pos1.z == pos2.z
-    elseif pos1.z ~= pos2.z then
-        return pos1.x == pos2.x and pos1.y == pos2.y
-    else
-        return true
-    end
 end
 
 ---Get a point by it's id
@@ -151,13 +162,17 @@ end
 ---@param weight? number weight of the link
 function Map:add(p1, p2, weight)
     log:debug('Add point', p1, 'and', p2)
-    assert(is_inline(p1, p2), 'p1 is not inline with p2')
 
+    -- Get by x, y, z instead of id to support vec and auto-add missing points
     local g_p1 = self:point(p1.x, p1.y, p1.z)
     local g_p2 = self:point(p2.x, p2.y, p2.z)
 
+    -- TODO automatic weight from distance
+
+    assert(g_p1:inline(g_p2), 'p1 is not inline with p2')
+
+    -- Creates link in both directions
     g_p1:link(g_p2, weight)
-    g_p2:link(g_p1, weight)
 
     -- TODO check for links adjacent to p2
     -- TODO should check include p1?
--- tests/test_map.lua
+++ tests/test_map.lua
@@ -28,6 +28,13 @@ function test.point_new()
     expect_eq(0, table_size(point.links))
 end
 
+function test.point_from_vec3()
+    local point = Point:from_vec3(Vec3:new(1, 2, 3))
+    expect_eq(1, point.x)
+    expect_eq(2, point.y)
+    expect_eq(3, point.z)
+end
+
 function test.point_link()
     local p1 = Point:new(1, 0, 0)
     local p2 = Point:new(2, 0, 0)
@@ -47,6 +54,20 @@ function test.point_link()
     expect_eq(1, p3.links['2,0,0'])
 end
 
+function test.point_is_inline()
+    local point = Point:new(0, 0, 0)
+    assert_true(point:inline(Point:new(1, 0, 0)))
+    assert_true(point:inline(Point:new(0, 1, 0)))
+    assert_true(point:inline(Point:new(0, 0, 1)))
+
+    assert_true(point:inline(Point:new(0, 0, 0)))
+
+    assert_false(point:inline(Point:new(1, 1, 0)))
+    assert_false(point:inline(Point:new(1, 0, 1)))
+    assert_false(point:inline(Point:new(0, 1, 1)))
+    assert_false(point:inline(Point:new(1, 1, 1)))
+end
+
 function test.point_to_string()
     local point = Point:new(1, 2, 3)
     expect_eq('Point(id="1,2,3",x=1,y=2,z=3,#links=0)', tostring(point))
@@ -60,8 +81,61 @@ end
 function test.map_new()
     local map = Map:new()
     expect_eq(0, table_size(map.graph))
+end
 
-    -- TODO everything else
+function test.map_load()
+    -- TODO
+end
+
+function test.map_dump()
+    -- TODO
+end
+
+function test.map_get()
+    local map = Map:new()
+    local point = Point:new(1, 2, 3)
+    map.graph[point.id] = point
+    assert_eq(point, map:get(point.id))
+end
+
+function test.map_point()
+    local map = Map:new()
+    local point = Point:new(1, 2, 3)
+    map.graph[point.id] = point
+    assert_eq(point, map:point(1, 2, 3))
+end
+
+function test.map_new_point()
+    local map = Map:new()
+    local point = map:point(1, 2, 3)
+    assert_eq('1,2,3', point.id)
+    assert_eq(1, point.x)
+    assert_eq(2, point.y)
+    assert_eq(3, point.z)
+    assert_eq(0, table_size(point.links))
+end
+
+function test.map_add()
+    local map = Map:new()
+    local p1 = Point:new(0, 0, 0)
+    local p2 = Point:new(1, 0, 0)
+
+    map:add(p1, p2, 1)
+
+    expect_eq(p1.id, map.graph[p1.id].id)
+    expect_eq(p2.id, map.graph[p2.id].id)
+
+    -- p1 and p2 are copied, so they can't be used directly here
+    expect_eq(1, map:get(p1.id).links[p2.id])
+    expect_eq(1, map:get(p2.id).links[p1.id])
+end
+
+function test.map_add_not_inline()
+    local map = Map:new()
+    local p1 = Point:new(0, 0, 0)
+    local p2 = Point:new(1, 1, 0)
+    local success = pcall(map.add, map, p1, p2)
+    expect_false(success)
 end
 
 return test
