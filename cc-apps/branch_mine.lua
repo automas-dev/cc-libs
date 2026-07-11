e375765ab2b26353e3b55e87ecfd68b0daac96c1
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-11 15:48:52 -0400

    Remove changes to branch_mine

--- cc-apps/branch_mine.lua
+++ cc-apps/branch_mine.lua
@@ -3,7 +3,7 @@ package.path = '../?.lua;../?/init.lua;' .. package.path
 local logging = require 'cc-libs.util.logging'
 logging.basic_config {
     level = logging.Level.INFO,
-    file_level = logging.Level.DEBUG,
+    file_level = logging.Level.TRACE,
     filepath = 'logs/branch_mine.log',
 }
 local log = logging.get_logger('main')
@@ -23,9 +23,6 @@ local Compass = ccl_location.Compass
 
 local ccl_nav = require 'cc-libs.turtle.nav'
 local Nav = ccl_nav.Nav
-
-local ccl_telemetry = require 'cc-libs.net.telemetry'
-local get_telemetry = ccl_telemetry.get_telemetry
 
 local argparse = require 'cc-libs.util.argparse'
 local parser = argparse.ArgParse:new(
@@ -56,9 +53,6 @@ end
 local location = Location:new(map)
 local tmc = Motion:new(location)
 local nav = Nav:new(map, location)
-
-local telem = get_telemetry()
-telem:set_location(location)
 
 local function debug_location()
     log:debug('Location is x=', location.pos.x, 'z=', location.pos.z, 'heading=', location:heading_name())
@@ -265,11 +259,6 @@ local function main()
     -- assert_torch() -- Disabled because of torch re-stock on dump
     assert_fuel()
 
-    -- Use relative heading for navigation if gps isn't available for heading
-    if not location.has_fix then
-        location.has_heading = true
-    end
-
     -- Move to start
 
     tmc:up()
@@ -372,4 +361,4 @@ local function main()
     log:info('Done!')
 end
 
-telem:run_parallel_with(log.catch_errors, log, main)
+log:catch_errors(main)
