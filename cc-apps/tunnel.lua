59165fc500a5902be36e85a344e92b7ed40fe706
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-14 13:53:04 -0400

    Add tunnel using new Telemetry:make_routine

---
+++ cc-apps/tunnel.lua
@@ -0,0 +1,143 @@
+package.path = '../?.lua;../?/init.lua;' .. package.path
+
+-- Import and configure logging
+local logging = require 'cc-libs.util.logging'
+logging.basic_config {
+    level = logging.Level.INFO,
+    file_level = logging.Level.DEBUG,
+    filepath = 'logs/tunnel.log',
+}
+local log = logging.get_logger('main')
+
+local ccl_map = require 'cc-libs.map'
+local Map = ccl_map.Map
+
+local ccl_location = require 'cc-libs.turtle.location'
+local Location = ccl_location.Location
+
+local ccl_motion = require 'cc-libs.turtle.motion'
+local Motion = ccl_motion.Motion
+
+local ccl_nav = require 'cc-libs.turtle.nav'
+local Nav = ccl_nav.Nav
+
+local ccl_telemetry = require 'cc-libs.net.telemetry'
+local get_telemetry = ccl_telemetry.get_telemetry
+
+local ccl_planner = require 'cc-libs.turtle.planner'
+local Planner = ccl_planner.Planner
+
+local argparse = require 'cc-libs.util.argparse'
+local parser = argparse.ArgParse:new('tunnel', 'Dig a 3x3 tunnel')
+parser:add_arg('length', { help = 'length of the tunnel' })
+local args = parser:parse_args({ ... })
+
+local length = args.length
+
+local map = Map:new()
+local location = Location:new(map)
+local tmc = Motion:new(location)
+tmc:enable_dig()
+local nav = Nav:new(map, tmc)
+
+local telem = get_telemetry()
+telem:set_location(location)
+tmc:attach_telemetry(telem)
+
+local lineup_start = telem:make_routine('lineup_start', function()
+    nav:mark_poi('start')
+    if not tmc:up() then
+        return false
+    end
+    tmc:right()
+    if not tmc:forward() then
+        return false
+    end
+    tmc:left()
+end)
+
+---Mine and move forward, then mine up and down if blocks exist
+---@return boolean success
+local mine_step = telem:make_routine('mine_step', function()
+    if turtle.detect() then
+        turtle.dig()
+    end
+    if not tmc:forward() then
+        return false
+    end
+    if turtle.detectUp() then
+        turtle.digUp()
+    end
+    map:link(map:pos(location.pos), map:point(location.pos.x, location.pos.y + 1, location.pos.z))
+    if turtle.detectDown() then
+        turtle.digDown()
+    end
+    map:link(map:pos(location.pos), map:point(location.pos.x, location.pos.y - 1, location.pos.z))
+    return true
+end)
+
+---Mine forward n block mining up and down along the path
+---@param n number
+---@return boolean success
+local mine_line = telem:make_routine('mine_layer', function(n)
+    log:debug(type(n))
+    for _ = 1, n do
+        if not mine_step() then
+            return false
+        end
+    end
+    return true
+end)
+
+---Mine forward n block mining up and down along the path
+---@param direction 'left'|'right'
+---@return boolean success
+local turn_to_next = telem:make_routine('turn_to_next', function(direction)
+    if direction == 'left' then
+        tmc:left()
+    elseif direction == 'right' then
+        tmc:right()
+    end
+    if not tmc:forward() then
+        return false
+    end
+    turtle.digUp()
+    turtle.digDown()
+    if direction == 'left' then
+        tmc:left()
+    elseif direction == 'right' then
+        tmc:right()
+    end
+    return true
+end)
+
+local return_to_start = telem:make_routine('return_to_start', function()
+    map:dump('tunnel_map.json')
+    local path = nav:find_path('start')
+    log:debug('Path is', path)
+    nav:follow_path(path)
+end)
+
+-- Main function
+local function main()
+    if location.has_fix and not location.has_heading then
+        if not tmc:forward() or not tmc:backward() then
+            error('Motion to acquire heading failed')
+        end
+    end
+
+    local start_heading = location.heading
+
+    lineup_start()
+    mine_line(length)
+    turn_to_next('left')
+    mine_line(length)
+    turn_to_next('right')
+    mine_line(length)
+    return_to_start()
+
+    tmc:face(start_heading)
+end
+
+-- Call main and log an error if raised
+telem:run_parallel_with('main', log.catch_errors, log, main)
