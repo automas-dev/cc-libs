7da6210b30d307b758e2c8f42c78b23d56d7e1f6
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-18 15:01:48 -0400

    Squash branch_mine

--- cc-libs/turtle/location.lua
+++ cc-libs/turtle/location.lua
@@ -194,11 +194,108 @@ function Location:update(action)
     end
 end
 
+---@class LocalFrame
+---@field origin Vec3
+---@field heading Compass
+---@field heading_diff number
+local LocalFrame = {}
+
+---Create a new LocalFrame
+---@param origin Vec3 position in global frame of local frame origin
+---@param heading? Compass heading of the frame, defaults to NORTH
+---@return LocalFrame
+function LocalFrame:new(origin, heading)
+    heading = heading or Compass.NORTH
+    local diff = heading - 1
+    local o = {
+        origin = origin,
+        heading = heading,
+        heading_diff = diff,
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+---Rotate a vector around the y axis
+---@param origin Vec3
+---@param vec Vec3
+---@param curr_heading Compass
+---@param target_heading Compass
+---@return Vec3
+local function rotate_around_y(origin, vec, curr_heading, target_heading)
+    if curr_heading == target_heading then
+        return vec
+    end
+    vec = vec - origin
+    local heading_diff = target_heading - curr_heading
+    assert(math.abs(heading_diff) < 4)
+    if heading_diff < 0 then
+        heading_diff = heading_diff + 4
+    end
+    assert(math.abs(heading_diff) > 0)
+    assert(math.abs(heading_diff) < 4)
+    local turn_right = heading_diff == 1
+    local turn_around = heading_diff == 2
+    local turn_left = heading_diff == 3
+    if turn_right then
+        local old_x = vec.x
+        vec.x = -vec.z
+        vec.z = old_x
+    elseif turn_around then
+        vec.x = -vec.x
+        vec.z = -vec.z
+    elseif turn_left then
+        local old_x = vec.x
+        vec.x = vec.z
+        vec.z = -old_x
+    end
+    return vec + origin
+end
+
+---Convert global position and heading to local
+---@param pos Vec3 position in global frame
+---@param heading? Compass heading in global frame, default assumes North
+---@return Vec3 local position in local frame
+---@return Compass heading heading in local frame
+function LocalFrame:to_local(pos, heading)
+    local local_pos = pos - self.origin
+    heading = heading or Compass.NORTH
+    local local_heading = heading + self.heading_diff
+    local_pos = rotate_around_y(Vec3:new(0, 0, 0), local_pos, heading, local_heading)
+    if local_heading < 1 then
+        local_heading = local_heading + 4
+    elseif local_heading > 4 then
+        local_heading = local_heading - 4
+    end
+    return local_pos, local_heading
+end
+
+---Convert local position and heading to global
+---@param pos Vec3 position in local frame
+---@param heading? Compass heading in local frame
+---@return Vec3 local position in global frame
+---@return Compass? heading heading in global frame
+function LocalFrame:to_global(pos, heading)
+    local global_pos = pos + self.origin
+    local heading_diff = self.heading - 1
+    local global_heading = heading + heading_diff
+    heading = heading or Compass.NORTH
+    global_pos = rotate_around_y(self.origin, global_pos, heading, Compass.NORTH)
+    if global_heading < 1 then
+        global_heading = global_heading + 4
+    elseif global_heading > 4 then
+        global_heading = global_heading - 4
+    end
+    return global_pos, global_heading
+end
+
 local M = {
     Compass = Compass,
     CompassName = static_name,
     Action = Action,
     Location = Location,
+    LocalFrame = LocalFrame,
     ---@type Location?
     _location = nil,
 }
