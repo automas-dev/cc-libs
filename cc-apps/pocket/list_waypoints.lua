645b90d31bfcf0021fb615625af1df5fce139ea5
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-19 10:45:18 -0400

    Add get_waypoint and list_waypoints scripts

---
+++ cc-apps/pocket/list_waypoints.lua
@@ -0,0 +1,30 @@
+package.path = '../../?.lua;../../?/init.lua;' .. package.path
+local logging = require 'cc-libs.util.logging'
+logging.basic_config {
+    level = logging.Level.INFO,
+    file_level = logging.Level.DEBUG,
+    filepath = 'logs/list_waypoints.log',
+}
+local log = logging.get_logger('main')
+
+-- TODO argparse commands to get and set waypoints, have option to use gps
+
+local ccl_map = require 'cc-libs.map'
+local MapClient = ccl_map.MapClient
+
+local GPS_TIMEOUT = 2
+
+local function main()
+    client = MapClient:new('server')
+
+    local waypoints = client:list_waypoints()
+    if waypoints == nil then
+        log:error('Failed to get waypoints from server')
+    else
+        for _, waypoint in ipairs(waypoints) do
+            print(waypoint.name, waypoint.waypoint.id)
+        end
+    end
+end
+
+log:catch_errors(main)
