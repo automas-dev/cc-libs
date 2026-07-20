c55536bbb0dc12d83d5fac655baf2f798974aabc
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-20 09:12:02 -0400

    Export TSLexer and TSParser

--- cc-libs/turtle/script/init.lua
+++ cc-libs/turtle/script/init.lua
@@ -1,1 +1,7 @@
-return {}
+local lexer = require 'cc-libs.turtle.script.lexer'
+local parser = require 'cc-libs.turtle.script.parser'
+
+return {
+    TSLexer = lexer.TSLexer,
+    TSParser = parser.TSParser,
+}
