d7e6297112476a10576fb5c2df1c87def3c4a07e
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-20 18:54:25 -0400

    WIP context, m start in ts/layer.txt area isn't working

--- cc-apps/turtle_script.lua
+++ cc-apps/turtle_script.lua
@@ -17,9 +17,6 @@ parser:add_arg('cmd', { help = 'command string, leave empty for repl', required 
 local args = parser:parse_args({ ... })
 
 local ccl_ts = require 'cc-libs.turtle.script'
-local TSLexer = ccl_ts.TSLexer
-local TSParser = ccl_ts.TSParser
-local TSTokenType = ccl_ts.TSTokenType
 local TSContext = ccl_ts.TSContext
 
 local ccl_motion = require 'cc-libs.turtle.motion'
@@ -49,9 +46,6 @@ local telem = get_telemetry()
 telem:set_location(location)
 tmc:attach_telemetry(telem)
 
----@type { [string]: TSToken[] }
-local fn_def = {}
-
 local function print_help()
     local lines = 'combine commands with a space, numbers following commands will repeat them\n'
         .. 'f forward\n'
@@ -72,125 +66,50 @@ local function print_help()
     textutils.pagedPrint(lines, height - 2)
 end
 
----Parse a command string
----@param cmd string
----@return TSToken[] program
-local function parse_cmd(cmd)
-    local ts_parser = TSParser:new()
-    ts_parser:takes_arg('m')
-    ts_parser:takes_arg('mark')
-    ts_parser:takes_arg('g')
-    ts_parser:takes_arg('goto')
-    local prog = ts_parser:parse(cmd)
-    return prog
-end
+local context = TSContext:new(tmc, nav)
 
----@param prog TSToken[]
----@param active_fn_stack string[]
----@return boolean success
-local function run_prog(prog, active_fn_stack)
-    if active_fn_stack == nil then
-        active_fn_stack = {}
+context:register('f', false, tmc.forward)
+context:register('b', false, tmc.backward)
+context:register('l', false, tmc.left)
+context:register('r', false, tmc.right)
+context:register('u', false, tmc.up)
+context:register('d', false, tmc.down)
+context:register('enable', false, tmc.enable_dig)
+context:register('disable', false, tmc.disable_dig)
+context:register('m', true, function(_, _, poi_name)
+    assert(poi_name ~= nil, 'missing arg')
+    return pcall(nav.mark_poi, nav, poi_name)
+end)
+context:register('g', true, function(_, _, poi_name)
+    assert(poi_name ~= nil, 'missing arg')
+    if nav:get_poi(poi_name) == nil then
+        log:warning('poi', poi_name, 'is missing')
+        if nav.map:get_waypoint(poi_name) == nil then
+            error('Missing poi ' .. tostring(poi_name))
+        end
+        nav:poi_from_waypoint(poi_name)
+        log:info('Got poi from waypoint', poi_name)
     end
-    log:debug('function stack is', active_fn_stack)
-    for i, step in ipairs(prog) do
-        if step.type == TSTokenType.DEF then
-            fn_def[step.name] = step.children
-        end
-        if step.type == TSTokenType.LOOP then
-            for _ = 1, step.count do
-                if not run_prog(step.children, active_fn_stack) then
-                    return false
-                end
-            end
-        end
-        if step.type == TSTokenType.CALL then
-            for _, s in ipairs(active_fn_stack) do
-                if s == step.name then
-                    error(
-                        'Recursion detected in function '
-                            .. tostring(active_fn_stack[#active_fn_stack])
-                            .. ' when calling '
-                            .. tostring(step.name)
-                    )
-                end
-            end
-            if step.name ~= 'q' then
-                log:info('Step', i, 'is', step.name, step.arg, step.count)
-            end
-            if fn_def[step.name] ~= nil then
-                log:debug('call fn', step.name)
-                table.insert(active_fn_stack, step.name)
-                local len_before = #active_fn_stack
-                for _ = 1, step.count do
-                    if not run_prog(fn_def[step.name], active_fn_stack) then
-                        return false
-                    end
-                end
-                assert(#active_fn_stack == len_before, 'Unbalanced function stack')
-                table.remove(active_fn_stack)
-            elseif step.name == 'f' then
-                if not tmc:forward(step.count) then
-                    return false
-                end
-            elseif step.name == 'b' then
-                if not tmc:backward(step.count) then
-                    return false
-                end
-            elseif step.name == 'u' then
-                if not tmc:up(step.count) then
-                    return false
-                end
-            elseif step.name == 'd' then
-                if not tmc:down(step.count) then
-                    return false
-                end
-            elseif step.name == 'l' then
-                tmc:left(step.count)
-            elseif step.name == 'r' then
-                tmc:right(step.count)
-            elseif step.name == 'enable' then
-                tmc:enable_dig()
-            elseif step.name == 'disable' then
-                tmc:disable_dig()
-            elseif step.name == 'm' or step.name == 'mark' then
-                local poi_name = step.arg
-                assert(poi_name ~= nil)
-                nav:mark_poi(poi_name)
-            elseif step.name == 'g' or step.name == 'goto' then
-                local poi_name = step.arg
-                assert(poi_name ~= nil)
-                if nav:get_poi(poi_name) == nil then
-                    log:warning('poi', poi_name, 'is missing')
-                    if nav.map:get_waypoint(poi_name) == nil then
-                        error('Missing poi ' .. tostring(poi_name))
-                    end
-                    nav:poi_from_waypoint(poi_name)
-                    log:info('Got poi from waypoint', poi_name)
-                end
-                local success, path = pcall(nav.find_path, nav, poi_name)
-                if not success then
-                    log:error('Failed to find path to poi', poi_name)
-                    return false
-                elseif #path < 2 then
-                    log:error('Path is empty to poi', poi_name)
-                    return false
-                end
-                nav:follow_path(path)
-            else
-                error('Unknown step ' .. tostring(step.name))
-            end
-        end
+    local success, path = pcall(nav.find_path, nav, poi_name)
+    if not success then
+        log:error('Failed to find path to poi', poi_name)
+        return false
+    elseif #path < 2 then
+        log:error('Path is empty to poi', poi_name)
+        return false
     end
+    nav:follow_path(path)
     return true
-end
+end)
+-- context:register('face', true, function(_, _, dir)
+--     if dir ~= 'front' and dir ~= 'back' and dir ~= 'left' and dir ~= 'right' then
+--         return false
+--     end
+--     return true
+-- end)
 
 local function main()
-    local prog = parse_cmd(table.concat(args.cmd, ' '))
-    if prog == nil then
-        error('Failed to compile program')
-    end
-    local success, err = pcall(run_prog, prog)
+    local success, err = context:exec(table.concat(args.cmd, ' '))
     if not success then
         log:error('Program failed', err)
     end
@@ -207,17 +126,11 @@ local function repl()
             log:info('Exiting')
             break
         elseif #cmd > 0 then
-            local success, prog = pcall(parse_cmd, cmd)
+            table.insert(history, cmd)
+            log:trace('History now', history)
+            local success, err = context:exec(cmd)
             if not success then
-                log:error('Failed to parse program', prog)
-            elseif prog ~= nil then
-                table.insert(history, cmd)
-                log:trace('History now', history)
-                local err
-                success, err = pcall(run_prog, prog)
-                if not success then
-                    log:error('Program failed', err)
-                end
+                log:error('Program failed', err)
             end
         end
     end
--- cc-libs/turtle/script/context.lua
+++ cc-libs/turtle/script/context.lua
@@ -1,8 +1,11 @@
+local logging = require 'cc-libs.util.logging'
+local log = logging.get_logger('turtle.script.context')
+
 local lexer = require 'cc-libs.turtle.script.parser'
 local TSParser = lexer.TSParser
 local TSTokenType = lexer.TSTokenType
 
----@alias TSFunction fun(motion: Motion, count: number, arg: string?, nav: Nav): boolean
+---@alias TSFunction fun(motion: Motion, count: number, arg: string?): boolean, string?
 
 ---@class TSContext
 ---@field motion Motion
@@ -29,95 +32,105 @@ function TSContext:new(motion, nav)
     }
     setmetatable(o, self)
     self.__index = self
-    -- TODO register default functions
     return o
-end
-
-function TSContext:register_defaults()
-    self:register('f', false, self.motion.forward)
-    self:register('b', false, self.motion.backward)
-    self:register('l', false, self.motion.left)
-    self:register('r', false, self.motion.right)
-    self:register('u', false, self.motion.up)
-    self:register('d', false, self.motion.down)
-    self:register('enable', false, self.motion.enable_dig)
-    self:register('disable', false, self.motion.disable_dig)
-    self:register('m', true, function(_, _, poi_name, nav)
-        assert(arg ~= nil)
-        return pcall(nav.mark_poi, nav, poi_name)
-    end)
-    self:register('g', true, function(_, _, poi_name, nav)
-        assert(arg ~= nil)
-        if nav:get_poi(poi_name) == nil then
-            if nav.map:get_waypoint(poi_name) == nil then
-                -- error('Missing poi ' .. tostring(poi_name))
-                return false
-            end
-            nav:poi_from_waypoint(poi_name)
-        end
-        local success, path = pcall(nav.find_path, nav, poi_name)
-        if not success then
-            return false
-        elseif #path < 2 then
-            return false
-        end
-        nav:follow_path(path)
-        return true
-    end)
 end
 
 ---Register a function for name
 ---@param name string
 ---@param fn TSFunction
 function TSContext:register(name, takes_arg, fn)
+    log:debug('Registering native function', name, takes_arg)
     self.native[name] = fn
     if takes_arg then
-        self.parser:does_token_take_arg(name)
+        log:trace('native function', name, 'takes an argument')
+        self.parser:takes_arg(name)
     end
 end
 
 ---Evaluate a single node in the ast
 ---@param node TSToken
 ---@return boolean success
+---@return string? error
 function TSContext:eval(node)
+    -- TODO trace
+    log:trace('eval', node)
+    -- Define a Function
     if node.type == TSTokenType.DEF then
-        assert(node.children ~= nil)
-        assert(node.arg ~= nil)
-        assert(self.native[node.arg] == nil, 'redefining native function ' .. tostring(node.arg))
-        self.defs[node.arg] = node.children
+        assert(node.children ~= nil, 'def has no children')
+        log:debug('Defining function', node.name, 'with', #node.children, 'children')
+        assert(self.native[node.name] == nil, 'redefining native function ' .. tostring(node.name))
+        self.defs[node.name] = node.children
+    -- Function Call
     elseif node.type == TSTokenType.CALL then
-        assert(node.arg ~= nil)
-        local fn_name = node.arg
+        local fn_name = node.name
+        -- Native Function
         local fn = self.native[fn_name]
         if fn ~= nil then
+            log:debug('Evaluating native call', fn_name, node.count, node.arg)
+
             table.insert(self.call_stack, fn_name)
             local stack_count = #self.call_stack
-            local res = fn(self.motion, node.count, node.arg, self.nav)
+            log:trace('call stack before is', self.call_stack)
+
+            local success, err = fn(self.motion, node.count, node.arg)
+
+            log:trace('call stack after is', self.call_stack)
             assert(#self.call_stack == stack_count, 'Unbalanced call')
             table.remove(self.call_stack)
-            return res
+
+            log:debug('Result of call', fn_name, 'is', success, err)
+            -- Handle nil as success
+            if success == false then
+                return false, 'native function failed ' .. tostring(fn_name)
+            end
+            return true
         end
+        -- Defined Function
         local def = self.defs[fn_name]
         if def ~= nil then
+            log:debug('Evaluating call', fn_name, node.count)
+
             table.insert(self.call_stack, fn_name)
+            log:trace('call stack before is', self.call_stack)
             local stack_count = #self.call_stack
-            local res = false
+
+            local success = false
+            local err = nil
+            -- Loop function count times
             for _, child in ipairs(def) do
-                res = self:eval(child)
-                if not res then
+                log:trace('Loop', _, 'for function', fn_name)
+                success, err = self:eval(child)
+                -- Stop if any call fails
+                if not success then
                     break
                 end
             end
+            log:trace('call stack after is', self.call_stack)
+
             assert(#self.call_stack == stack_count, 'Unbalanced call')
             table.remove(self.call_stack)
-            return res
+
+            log:debug('Result of call', fn_name, 'is', success, err)
+            -- Handle nil as success
+            if success == false then
+                return false, 'function failed ' .. tostring(fn_name)
+            end
+            return true
         end
-        return false
+        return false, 'function not defined ' .. tostring(fn_name)
+    -- Execute multiple nodes count times
     elseif node.type == TSTokenType.LOOP then
         assert(node.children ~= nil)
-        for _, child in ipairs(node.children) do
-            if not self:eval(child) then
-                return false
+        log:debug('Evaluating loop')
+        -- Loop count times
+        for _ = 1, node.count do
+            -- Run each node in loop
+            for _, child in ipairs(node.children) do
+                local success, err = self:eval(child)
+                -- Return error if a call fails
+                if not success then
+                    return false, err
+                end
             end
         end
         return true
@@ -130,11 +143,13 @@ end
 ---Execute a command string
 ---@param text string
 ---@return boolean success
+---@return string? error
 function TSContext:exec(text)
     local ast = self.parser:parse(text)
     for _, node in ipairs(ast) do
-        if not self:eval(node) then
-            return false
+        local success, err = self:eval(node)
+        if not success then
+            return false, err
         end
     end
     return true
