24357e5e6dcdc129850e6b0dfaff8af525143ec5
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-20 10:23:49 -0400

    Replace turtle_script and turtle_script2 with turtle_script_3

--- cc-apps/turtle_script.lua
+++ cc-apps/turtle_script.lua
@@ -16,6 +16,11 @@ local parser =
 parser:add_arg('cmd', { help = 'command string, leave empty for repl', required = false, is_multi = true })
 local args = parser:parse_args({ ... })
 
+local ccl_ts = require 'cc-libs.turtle.script'
+local TSLexer = ccl_ts.TSLexer
+local TSParser = ccl_ts.TSParser
+local TSTokenType = ccl_ts.TSTokenType
+
 local ccl_motion = require 'cc-libs.turtle.motion'
 local Motion = ccl_motion.Motion
 
@@ -43,6 +48,9 @@ local telem = get_telemetry()
 telem:set_location(location)
 tmc:attach_telemetry(telem)
 
+---@type { [string]: TSToken[] }
+local fn_def = {}
+
 local function print_help()
     local lines = 'combine commands with a space, numbers following commands will repeat them\n'
         .. 'f forward\n'
@@ -63,164 +71,116 @@ local function print_help()
     textutils.pagedPrint(lines, height - 2)
 end
 
-local function is_valid(cmd)
-    local valid = { '[', ']', 'f', 'b', 'l', 'r', 'u', 'd', 'enable', 'disable', 'm', 'mark', 'g', 'goto' }
-    for _, v in ipairs(valid) do
-        if cmd == v then
-            return true
-        end
-    end
-    return false
-end
-
 ---Parse a command string
 ---@param cmd string
----@return [string, number|string][]? program
+---@return TSToken[] program
 local function parse_cmd(cmd)
-    -- TODO replace with str.tokenize when it works
-    local all_parts = {}
-    for w in cmd:gmatch('%S+') do
-        table.insert(all_parts, w)
-    end
-
-    if #all_parts == 0 then
-        return
-    end
-
-    log:debug('all parts are', all_parts)
-
-    local actions = {}
-    local nest = {}
-
-    local i = 1
-    while i <= #all_parts do
-        local w = all_parts[i]
-        if not is_valid(w) then
-            log:error('Invalid command', w)
-            return
-        end
-        if w == '[' then
-            table.insert(nest, actions)
-            actions = {}
-        elseif w == ']' then
-            if #nest == 0 then
-                log:error('Unclosed [')
-                return
-            end
-            local nest_actions = actions
-            actions = table.remove(nest)
-            local count = tonumber(all_parts[i + 1])
-            if count == nil then
-                count = 1
-            else
-                i = i + 1
-            end
-            for _ = 1, count do
-                for _, v in ipairs(nest_actions) do
-                    table.insert(actions, v)
-                end
-            end
-        elseif w == 'm' or w == 'mark' then
-            local name = all_parts[i + 1]
-            if name == nil then
-                log:error('Invalid marker name', name)
-                return
-            end
-            table.insert(actions, { 'm', name })
-            i = i + 1
-        elseif w == 'g' or w == 'goto' then
-            local name = all_parts[i + 1]
-            if name == nil then
-                log:error('Invalid marker name', name)
-                return
-            end
-            table.insert(actions, { 'g', name })
-            i = i + 1
-        else
-            local count = tonumber(all_parts[i + 1])
-            if count == nil then
-                count = 1
-                while all_parts[i + 1] == w do
-                    count = count + 1
-                    i = i + 1
-                end
-            else
-                i = i + 1
-            end
-            table.insert(actions, { w, count })
-        end
-        i = i + 1
-    end
-
-    return actions
+    local ts_lexer = TSLexer:new(cmd)
+    local ts_parser = TSParser:new(ts_lexer)
+    ts_parser:takes_arg('m')
+    ts_parser:takes_arg('mark')
+    ts_parser:takes_arg('g')
+    ts_parser:takes_arg('goto')
+    local prog = ts_parser:parse()
+    return prog
 end
 
----@param prog [string, number|string][]
-local function run_prog(prog)
+---@param prog TSToken[]
+---@param active_fn_stack string[]
+---@return boolean success
+local function run_prog(prog, active_fn_stack)
+    if active_fn_stack == nil then
+        active_fn_stack = {}
+    end
+    log:debug('function stack is', active_fn_stack)
     for i, step in ipairs(prog) do
-        local step_cmd = step[1]
-        local step_arg = step[2]
-        if step_cmd ~= 'q' then
-            log:info('Step', i, 'is', step_cmd, step_arg)
+        if step.type == TSTokenType.DEF then
+            fn_def[step.name] = step.children
         end
-        if step_cmd == 'f' then
-            ---@cast step_arg number
-            if not tmc:forward(step_arg) then
-                break
-            end
-        elseif step_cmd == 'b' then
-            ---@cast step_arg number
-            if not tmc:backward(step_arg) then
-                break
-            end
-        elseif step_cmd == 'u' then
-            ---@cast step_arg number
-            if not tmc:up(step_arg) then
-                break
-            end
-        elseif step_cmd == 'd' then
-            ---@cast step_arg number
-            if not tmc:down(step_arg) then
-                break
-            end
-        elseif step_cmd == 'l' then
-            ---@cast step_arg number
-            tmc:left(step_arg)
-        elseif step_cmd == 'r' then
-            ---@cast step_arg number
-            tmc:right(step_arg)
-        elseif step_cmd == 'enable' then
-            tmc:enable_dig()
-        elseif step_cmd == 'disable' then
-            tmc:disable_dig()
-        elseif step_cmd == 'm' then
-            local poi_name = step[2]
-            ---@cast poi_name string
-            nav:mark_poi(poi_name)
-        elseif step_cmd == 'g' then
-            local poi_name = step[2]
-            ---@cast poi_name string
-            if nav:get_poi(poi_name) == nil then
-                log:warning('poi', poi_name, 'is missing')
-                if map:get_waypoint(poi_name) == nil then
-                    error('Missing poi ' .. tostring(poi_name))
+        if step.type == TSTokenType.LOOP then
+            for _ = 1, step.count do
+                if not run_prog(step.children, active_fn_stack) then
+                    return false
                 end
-                nav:poi_from_waypoint(poi_name)
-                log:info('Got poi from waypoint', poi_name)
             end
-            local success, path = pcall(nav.find_path, nav, poi_name)
-            if not success then
-                log:error('Failed to find path to poi', poi_name)
-                break
-            elseif #path < 2 then
-                log:error('Path is empty to poi', poi_name)
-                break
+        end
+        if step.type == TSTokenType.CALL then
+            for _, s in ipairs(active_fn_stack) do
+                if s == step.name then
+                    error(
+                        'Recursion detected in function '
+                            .. tostring(active_fn_stack[#active_fn_stack])
+                            .. ' when calling '
+                            .. tostring(step.name)
+                    )
+                end
             end
-            nav:follow_path(path)
-        else
-            error('Unknown step ' .. tostring(step_cmd))
+            if step.name ~= 'q' then
+                log:info('Step', i, 'is', step.name, step.arg, step.count)
+            end
+            if fn_def[step.name] ~= nil then
+                log:debug('call fn', step.name)
+                table.insert(active_fn_stack, step.name)
+                local len_before = #active_fn_stack
+                if not run_prog(fn_def[step.name], active_fn_stack) then
+                    return false
+                end
+                assert(#active_fn_stack == len_before, 'Unbalanced function stack')
+                table.remove(active_fn_stack)
+            elseif step.name == 'f' then
+                if not tmc:forward(step.count) then
+                    return false
+                end
+            elseif step.name == 'b' then
+                if not tmc:backward(step.count) then
+                    return false
+                end
+            elseif step.name == 'u' then
+                if not tmc:up(step.count) then
+                    return false
+                end
+            elseif step.name == 'd' then
+                if not tmc:down(step.count) then
+                    return false
+                end
+            elseif step.name == 'l' then
+                tmc:left(step.count)
+            elseif step.name == 'r' then
+                tmc:right(step.count)
+            elseif step.name == 'enable' then
+                tmc:enable_dig()
+            elseif step.name == 'disable' then
+                tmc:disable_dig()
+            elseif step.name == 'm' or step.name == 'mark' then
+                local poi_name = step.arg
+                assert(poi_name ~= nil)
+                nav:mark_poi(poi_name)
+            elseif step.name == 'g' or step.name == 'goto' then
+                local poi_name = step.arg
+                assert(poi_name ~= nil)
+                if nav:get_poi(poi_name) == nil then
+                    log:warning('poi', poi_name, 'is missing')
+                    if nav.map:get_waypoint(poi_name) == nil then
+                        error('Missing poi ' .. tostring(poi_name))
+                    end
+                    nav:poi_from_waypoint(poi_name)
+                    log:info('Got poi from waypoint', poi_name)
+                end
+                local success, path = pcall(nav.find_path, nav, poi_name)
+                if not success then
+                    log:error('Failed to find path to poi', poi_name)
+                    return false
+                elseif #path < 2 then
+                    log:error('Path is empty to poi', poi_name)
+                    return false
+                end
+                nav:follow_path(path)
+            else
+                error('Unknown step ' .. tostring(step.name))
+            end
         end
     end
+    return true
 end
 
 local function main()
@@ -235,9 +195,10 @@ local function main()
 end
 
 local function repl()
+    local history = {}
     while true do
         write('repl> ')
-        local cmd = read()
+        local cmd = read(nil, history)
         if cmd == 'h' or cmd == 'help' then
             print_help()
         elseif cmd == 'q' or cmd == 'quit' then
@@ -246,6 +207,8 @@ local function repl()
         elseif #cmd > 0 then
             local prog = parse_cmd(cmd)
             if prog ~= nil then
+                table.insert(history, cmd)
+                log:trace('History now', history)
                 local success, err = pcall(run_prog, prog)
                 if not success then
                     log:error('Program failed', err)
