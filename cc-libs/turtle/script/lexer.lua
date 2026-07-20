ca49575a85f8822b88c76c229c060162ac592ccd
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-19 21:58:36 -0400

    Add more functions to lexer

--- cc-libs/turtle/script/lexer.lua
+++ cc-libs/turtle/script/lexer.lua
@@ -30,6 +30,7 @@ end
 ---@param str string
 ---@return boolean
 function Lexer:match_at(i, str)
+    assert(#str > 0)
     local text = self.text:sub(i)
     if #text < #str then
         return false
@@ -38,6 +39,22 @@ function Lexer:match_at(i, str)
         text = text:sub(1, #str)
     end
     return text == str
+end
+
+---Check if any value in arr exists at position i in text
+---@param i number
+---@param arr string[] list of strings to match
+---@return boolean
+function Lexer:match_any_at(i, arr)
+    local text = self.text:sub(i)
+    for _, str in ipairs(arr) do
+        if #text >= #str then
+            if str == text:sub(1, #str) then
+                return true
+            end
+        end
+    end
+    return false
 end
 
 ---Take characters until a value
@@ -72,26 +89,87 @@ function Lexer:take_while(value)
     self.i = i
 end
 
--- ---Take characters until a value
--- ---@param values string[]
--- ---@return string token
--- function Lexer:take_until_any(values)
---     local i = self.i
---     while i <= self.len do
---         for _, val in ipairs(values) do
---             if self:match_at(i, val) then
---                 break
---             end
---         end
---         i = i + 1
---     end
---     if i == self.i then
---         return ''
---     end
---     local text = self.text:sub(self.i, i - 1)
---     self.i = i
---     return text
--- end
+---Progress the index while the current character is whitespace
+function Lexer:take_ws()
+    local i = self.i
+    while i <= self.len do
+        if not self:match_any_at(i, { ' ', '\t', '\r', '\n' }) then
+            break
+        end
+        i = i + 1
+    end
+    self.i = i
+end
+
+---Take characters that are a decimal digit
+---@return string value
+function Lexer:take_decimal()
+    local i = self.i
+    while i <= self.len do
+        if not self:match_any_at(i, { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }) then
+            break
+        end
+        i = i + 1
+    end
+    local text = self.text:sub(self.i, i - 1)
+    self.i = i
+    return text
+end
+
+---Take characters that make a number. This can start with a - for negative and include on . for floats
+---@return string value
+function Lexer:take_number()
+    local num = ''
+    if self:match_at(self.i, '-') then
+        num = '-'
+        self.i = self.i + 1
+    end
+    num = num .. self:take_decimal()
+    if self:match_at(self.i, '.') then
+        self.i = self.i + 1
+        local fraction = self:take_decimal()
+        num = num .. '.' .. fraction
+    end
+    return num
+end
+
+---Take characters of a quoted string starting on the opening quote
+---@return string value
+function Lexer:take_quoted_string()
+    assert(self:match_at(self.i, '"'))
+
+    self.i = self.i + 1
+    local i = self.i
+    while i <= self.len do
+        if self:match_at(i, '"') and not self:match_at(i - 1, '\\') then
+            break
+        end
+        i = i + 1
+    end
+    local text = self.text:sub(self.i, i - 1)
+    text = text:gsub('\\"', '"')
+    self.i = i + 1
+    return text
+end
+
+---Take characters until a value
+---@param values string[]
+---@return string token
+function Lexer:take_until_any(values)
+    local i = self.i
+    while i <= self.len do
+        if self:match_any_at(i, values) then
+            break
+        end
+        i = i + 1
+    end
+    if i == self.i then
+        return ''
+    end
+    local text = self.text:sub(self.i, i - 1)
+    self.i = i
+    return text
+end
 
 return {
     Lexer = Lexer,
--- tests/test_turtle_script_lexer.lua
+++ tests/test_turtle_script_lexer.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: invisible
+
 local ccl_ts_lexer = require 'cc-libs.turtle.script.lexer'
 local Lexer = ccl_ts_lexer.Lexer
 
@@ -9,6 +11,38 @@ function test.lexer_new()
     expect_eq(text, lexer.text)
     expect_eq(1, lexer.i)
     expect_eq(#text, lexer.len)
+end
+
+function test.lexer_sub()
+    local lexer = Lexer:new('hello world')
+    expect_eq('hello world', lexer:sub(1))
+    expect_eq('ello world', lexer:sub(2))
+    expect_eq('d', lexer:sub(11))
+    expect_eq('', lexer:sub(12))
+    expect_eq('', lexer:sub(13))
+end
+
+function test.lexer_match_at()
+    local lexer = Lexer:new('hello world')
+    expect_true(lexer:match_at(1, 'h'))
+    expect_true(lexer:match_at(1, 'he'))
+    expect_true(lexer:match_at(1, 'hello world'))
+
+    expect_false(lexer:match_at(2, 'h'))
+
+    expect_true(lexer:match_at(2, 'e'))
+    expect_true(lexer:match_at(2, 'el'))
+    expect_true(lexer:match_at(2, 'ello world'))
+    expect_true(lexer:match_at(11, 'd'))
+
+    expect_false(lexer:match_at(12, 'd'))
+end
+
+function test.lexer_match_any_at()
+    local lexer = Lexer:new('hello world')
+    expect_true(lexer:match_any_at(1, { 'h', 'w' }))
+    expect_false(lexer:match_any_at(2, { 'h', 'w' }))
+    expect_false(lexer:match_any_at(1, { '1' }))
 end
 
 function test.lexer_take_until()
@@ -30,11 +64,56 @@ function test.lexer_take_while()
     expect_eq(5, lexer.i)
 end
 
--- function test.lexer_take_until_any()
---     local lexer = Lexer:new('hello world')
---     local a = lexer:take_until_any({ 'e', 'l' })
---     expect_eq('hello', a)
---     expect_eq(6, lexer.i)
--- end
+function test.lexer_take_ws()
+    local lexer = Lexer:new('h world')
+    lexer:take_ws()
+    expect_eq(1, lexer.i)
+
+    lexer = Lexer:new(' \t\r\nhello world')
+    lexer:take_ws()
+    expect_eq(5, lexer.i)
+end
+
+function test.lexer_take_number()
+    expect_eq('100', Lexer:new('100'):take_number())
+    expect_eq('1', Lexer:new('1 2'):take_number())
+    expect_eq('1.0', Lexer:new('1.0'):take_number())
+    expect_eq('1.2', Lexer:new('1.2'):take_number())
+    expect_eq('-3', Lexer:new('-3'):take_number())
+    expect_eq('-3.14', Lexer:new('-3.14'):take_number())
+end
+
+function test.lexer_take_quoted_string()
+    local lexer = Lexer:new('"hello world"')
+    local text = lexer:take_quoted_string()
+    expect_eq('hello world', text)
+    expect_eq(14, lexer.i)
+end
+
+function test.lexer_take_quoted_string_with_escape()
+    local lexer = Lexer:new('"hello\\"world"')
+    local text = lexer:take_quoted_string()
+    expect_eq('hello"world', text)
+    expect_eq(15, lexer.i)
+end
+
+function test.lexer_take_quoted_string_escape_not_quote()
+    local lexer = Lexer:new('"hello\\ world"')
+    local text = lexer:take_quoted_string()
+    expect_eq('hello\\ world', text)
+    expect_eq(15, lexer.i)
+end
+
+function test.lexer_take_until_any()
+    local lexer = Lexer:new('hello world')
+    local a = lexer:take_until_any({ 'e', 'l' })
+    expect_eq('h', a)
+    expect_eq(2, lexer.i)
+
+    lexer = Lexer:new('hello world')
+    a = lexer:take_until_any({ 'o', 'l' })
+    expect_eq('he', a)
+    expect_eq(3, lexer.i)
+end
 
 return test
