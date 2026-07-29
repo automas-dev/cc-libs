badd9a0e68251c71625d2202e5e63f2e0068e465
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-29 18:17:39 -0400

    Smarter updated when linking existing points

--- cc-libs/map/map.lua
+++ cc-libs/map/map.lua
@@ -446,12 +446,14 @@ function Map:link(p1, p2, weight)
 
     local n_p1 = table_size(p1.links)
     local n_p2 = table_size(p2.links)
+    local w1 = p1.links[p2.id]
+    local w2 = p2.links[p1.id]
 
     link_points(p1, p2, weight)
-    if table_size(p1.links) > n_p1 then
+    if weight ~= w1 or table_size(p1.links) > n_p1 then
         self:update_remote_point(p1)
     end
-    if table_size(p2.links) > n_p2 then
+    if weight ~= w2 or table_size(p2.links) > n_p2 then
         self:update_remote_point(p2)
     end
 end
@@ -472,9 +474,13 @@ function Map:dir_link(p1, p2, weight)
 
     assert(inline(p1, p2), 'p1 is not inline with p2')
 
+    local n_p1 = table_size(p1.links)
+    local w1 = p1.links[p2.id]
+
     link_points(p1, p2, weight, true)
-    self:update_remote_point(p1)
-    self:update_remote_point(p2)
+    if weight ~= w1 or table_size(p1.links) > n_p1 then
+        self:update_remote_point(p1)
+    end
 end
 
 ---Link adjacent points if they exist
