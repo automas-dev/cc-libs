00779004a67f93d6e53008cedd2417ffaa701bee
Thomas Harrison <theharrisoncrafter@gmail.com>
2026-07-30 17:57:40 -0400

    Update type of return in argparse

--- cc-libs/util/argparse.lua
+++ cc-libs/util/argparse.lua
@@ -203,7 +203,7 @@ end
 
 ---Parse arguments and return their values.
 ---@param args string[] array of arguments to parse
----@return table table of args
+---@return { [string]: string } args table of values for each argument
 function ArgParse:parse_args(args)
     local result = {}
 
